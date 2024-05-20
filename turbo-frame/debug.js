1b6bedd63413871983e4b7bff09401220da2d48a
Paul Shippy <paul.shippy@strongmind.com>
2024-05-19 22:45:51 -0700

    Add custom logs for clicks, fetches and streams

--- turbo-frame/debug.js
+++ turbo-frame/debug.js
@@ -5,6 +5,9 @@ if (!window.turboFrameDebug) {
         constructor() {
             this.debugStylesheetPath = 'turbo-frame/debug.css';
             this.enabled = false;
+            this.handleTurboClick = this.handleTurboClick.bind(this);
+            this.handleTurboBeforeFetchResponse = this.handleTurboBeforeFetchResponse.bind(this);
+            this.handleTurboBeforeStreamRender = this.handleTurboBeforeStreamRender.bind(this);
         }
 
         toggleDebug() {
@@ -39,19 +42,65 @@ if (!window.turboFrameDebug) {
         }
 
         setupEventListeners() {
+            document.addEventListener('turbo:click', this.handleTurboClick)
             document.addEventListener('turbo:before-fetch-response', this.handleTurboBeforeFetchResponse)
-            console.log('Event listeners added')
+            document.addEventListener('turbo:before-stream-render', this.handleTurboBeforeStreamRender);
         }
 
         removeEventListeners() {
+            document.removeEventListener('turbo:click', this.handleTurboClick)
             document.removeEventListener('turbo:before-fetch-response', this.handleTurboBeforeFetchResponse)
-            console.log('Event listeners removed')
+            document.removeEventListener('turbo:before-stream-render', this.handleTurboBeforeStreamRender);
         }
 
+        handleTurboClick(event) {
+            const frameId = this.getFrameId(event.target);
+
+            console.log(`%c${event.target.innerText}`,
+                'font-weight: bold; text-decoration: underline;',
+                'click was intercepted by Turbo which will',
+                `replace the contents of #${frameId} with the contents of <turbo-frame id="${frameId}"></turbo-frame> returned from ${event.target.getAttribute('href')}`,
+                event)
+
+        }
 
         handleTurboBeforeFetchResponse(event) {
-            console.log('turbo:before-fetch-response', event)
+            const frameId = event.target.id;
+            if (frameId) {
+                console.log(`Turbo is replacing the contents of #${event.target.id} with the contents of <turbo-frame id="${frameId}"></turbo-frame> returned from ${event.target.getAttribute('src')}`,
+                    event);
+            }
+            else {
+                console.log(`Turbo is replacing the contents of the ${event.target.localName} element`,
+                    event);
+            }
         }
+
+        handleTurboBeforeStreamRender(event) {
+            let action = event.target.getAttribute('action');
+            if (action == 'before' || action == 'after') {
+                console.log(`Turbo has received a stream of html that will insert the contents of the template received ${action} the contents of #${event.target.getAttribute('target')}`, event)
+            }
+            else {
+                console.log(`Turbo has received a stream of html that will ${action} the contents of #${event.target.getAttribute('target')} with the contents of the template received`, event)
+            }
+
+        }
+
+        getFrameId(element) {
+            const frameId = element.getAttribute('data-turbo-frame');
+            if (!frameId) {
+                while (element.parentElement) {
+                    if (element.localName === 'turbo-frame') {
+                        const parentFrameId = element.id;
+                        return parentFrameId;
+                    }
+                    element = element.parentElement;
+                }
+            }
+            return frameId;
+        }
+
     }
 
     window.turboFrameDebug = new TurboFrameDebug();
