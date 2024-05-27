c64c68d5619c317e8f918d7421d8d1ca80e807e6
Paul Shippy <paul.shippy@strongmind.com>
2024-05-26 19:27:49 -0700

    Add on/off badge

--- background.js
+++ background.js
@@ -1,8 +1,26 @@
-chrome.action.onClicked.addListener(tab => {
-  if (!tab.url.includes('chrome://')) {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      files: ['turbo-frame/init.js']
-    })
+
+chrome.action.onClicked.addListener(async (tab) => {
+  if (tab.url.includes('chrome://')) return;
+
+  const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+  const nextState = prevState === 'ON' ? 'OFF' : 'ON'
+
+  chrome.action.setBadgeText({
+    text: nextState,
+    tabId: tab.id,
+  });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ['turbo-frame/init.js']
+  })
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'loading') {
+    chrome.action.setBadgeText({
+      text: "OFF",
+      tabId: tabId,
+    });
   }
-})
+});
